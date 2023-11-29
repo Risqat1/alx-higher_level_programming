a8ad6219f9c7cc6459a930eedb8ca09b77c95a1d
Risqat1 <Kolawolerisqat17@gmail.com>
2023-11-29 09:39:06 -0800

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+output = ""
 for i in range(0, 100):
-    if i == 99:
-        print("{}".format(i))
+    if i < 10:
+        output = output + "0" + str(i) + ", "
+    elif i < 99:
+        output = output + str(i) + ", "
     else:
-        print("{:02d}, ".format(i), end='')
+        output = output + str(i)
+print("{}".format(output))
+
