fcc4cb7b418dbedc492676c8c5ab0e0dcfc22403
Risqat1 <Kolawolerisqat17@gmail.com>
2023-11-29 06:08:09 -0800

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i in [101, 113]:
-        continue
-    print("{}".format(chr(i)), end='')
+for i in range(0, 100):
+    if i == 99:
+        print("{}".format(i))
+    else:
+        print("{:02d}, ".format(i), end='')
